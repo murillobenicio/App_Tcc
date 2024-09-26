24fc30de45b85dd4381b9a1c1d93dad6db2a9d61
murillobenicio <134696484+murillobenicio@users.noreply.github.com>
2024-09-26 12:48:18 +0000

    Eight_Commit

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 const StackLayout = () => {
   const { user } = useAuth();
   const segments = useSegments();
+  
 
   useEffect(() => {
     const inAuthGroup = segments[0] === "(protected)";
--- src/app/index.js
+++ src/app/index.js
@@ -1,28 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, BackHandler, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { BackHandler ,Button, StyleSheet, Text, View, TextInput} from 'react-native';
 import { useAuth } from '../hooks/Auth';
 import { router } from 'expo-router';
-import {Ionicons} from "@expo/vector-icons";
 import { useState } from 'react';
+import {Ionicons} from "@expo/vector-icons"
 
 export default function App() {
-  const {signIn, signOut} = useAuth();
-  const {email, setEmail} = useState("super@email.com");
-  const {password, setPassword} = useState("A123456a!");
-  const [passwordVisibility, setPasswordVisibility] = useState(false);
+  const { signIn, signOut } = useAuth();
+  const [email, setEmail] = useState("super@email.com");
+  const [password, setPassword] = useState("A123456a!");
+  const [passwordVisiblity, setPasswordVisibility] = useState(false);
 
   const tooglePasswordVisibility = () => {
-    setPasswordVisibility(!passwordVisibility);
-  }
+    setPasswordVisibility(!passwordVisiblity);
+  };
 
   const handleEntrarSuper = async () => {
-    console.log("Super email: ", email, "- password: ", password);
-
     try {
-      await signIn({email, password});
-      // router.replace("/");
+      await signIn({ email, password });
+       router.replace("/");
     } catch (error) {
-      Alert.alert("Error", error.message);
       console.log(error);
     }
   }
@@ -30,26 +27,40 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aplicativo Pronto para Usar</Text>
-    <View style={styles.inputbox}>
-      <Ionicons name="mail-open-outline" size={20} color="black"/>
-      <TextInput style={styles.emailinput} placeholder="E-mail" value={email} onChangeText={setEmail}/>
-    </View>
-    <View style={styles.inputbox}>
-      <Ionicons name="lock-closed-outline" size={20} color="black"/>
-      <TextInput style={styles.emailinput} placeholder="Senha" value={password} onChangeText={setPassword} secureTextEntry={passwordVisibility}/>
-      <Ionicons name={passwordVisibility ? "eye-off-outline" : "eye-outline"} size={20} color="black" onPress={tooglePasswordVisibility}/>
-    </View>
+      <View style={styles.inputBox}>
+        <Ionicons name="mail-open-outline" size={20} color="black" />
+        <TextInput
+          style={styles.emailinput}
+          placeholder="E-mail"
+          value={email}
+          onChange={setEmail}
+        />
+      </View>
 
-      <Button 
+      <View style={styles.inputBox}>
+        <Ionicons name="lock-closed-outline" size={20} color="black" />
+        <TextInput
+          style={styles.emailinput}
+          placeholder="Senha"
+          value={password}
+          onChange={setPassword}
+          secureTextEntry={passwordVisiblity}
+        />
+        <Ionicons name={passwordVisiblity ? "eye-off-outline" : "eye-outline"}
+          size={20}
+          color="black"
+          onPress={tooglePasswordVisibility} />
+      </View>
+      <Button
       style={styles.button}
-      title='Entrar'
-      onPress={handleEntrarSuper}
+        title='Entrar'
+        onPress={handleEntrarSuper}
       />
       <Button title='Sobre' onPress={() => router.push("about")} />
-        <Button
-         title='Sair do Aplicativo'
-         onPress={() => BackHandler.exitApp()}
-         />
+      <Button
+        title='Sair do Aplicativo'
+        onPress={() => BackHandler.exitApp()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -63,23 +74,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     gap: 15,
   },
-  title:{
+  title: {
     fontFamily: "regular",
-    fontSize:28,
+    fontSize: 28,
   },
-  inputbox:{
-    flexDirection:"row",
+  inputBox: {
+    flexDirection: "row",
     gap: 10,
     marginHorizontal: 40,
     marginVertical: 10,
-    alignItems: "center",
+    alignItems: "center"
   },
-  emailinput:{
-    flex:1,
+  emailinput: {
+    flex: 1,
     fontFamily: "regular",
     fontSize: 20,
   },
   button:{
     width: "100%",
+    width: 100,
   }
 });
